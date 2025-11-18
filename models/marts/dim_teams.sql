bc6d4f0fa74e40cb426772a6647d5aad4a7d96e1
Mateus Kasuya <mateusvbkasuya@gmail.com>
2025-11-18 10:08:11 -0300

    horario b2b game

--- models/marts/dim_teams.sql
+++ models/marts/dim_teams.sql
@@ -43,7 +43,12 @@ next_game_info AS (
         home_team_abbreviation,
         visitor_team_id,
         visitor_team_name,
-        visitor_team_abbreviation
+        visitor_team_abbreviation,
+        -- Determine if next game is at home
+        CASE 
+            WHEN team_id = home_team_id THEN TRUE
+            ELSE FALSE
+        END AS is_next_game_home
     FROM {{ ref('int_games_teams_pilled') }}
     WHERE is_next_game = true
     QUALIFY ROW_NUMBER() OVER (PARTITION BY team_id ORDER BY game_date ASC, game_id ASC) = 1
@@ -53,6 +58,7 @@ next_game_info AS (
 next_opponent_info AS (
     SELECT
         ngi.team_id,
+        ngi.is_next_game_home,
         CASE
             WHEN ngi.team_id = ngi.home_team_id THEN ngi.visitor_team_id
             WHEN ngi.team_id = ngi.visitor_team_id THEN ngi.home_team_id
@@ -86,11 +92,22 @@ dim_teams AS (
         noi.next_opponent_id,
         noi.next_opponent_name,
         noi.next_opponent_abbreviation,
+        noi.is_next_game_home,
         tlf_opp.team_last_five_games AS next_opponent_team_last_five_games,
         ts_opp.conference_rank AS next_opponent_conference_rank,
         tr_opp.team_rating_rank AS next_opponent_team_rating_rank,
         tr_opp.team_offensive_rating_rank AS next_opponent_team_offensive_rating_rank,
         tr_opp.team_defensive_rating_rank AS next_opponent_team_defensive_rating_rank,
+        
+        -- Injury report time for the team's home city (from staging)
+        t.team_injury_report_time_brasilia,
+        
+        -- Injury report time for the next game (based on where the game is played)
+        CASE 
+            WHEN noi.is_next_game_home THEN t.team_injury_report_time_brasilia
+            ELSE ts_opp.team_injury_report_time_brasilia
+        END AS next_game_injury_report_time_brasilia,
+        
         CURRENT_TIMESTAMP() AS loaded_at
     FROM {{ ref('stg_team_standings') }} AS t
     LEFT JOIN team_last_five AS tlf ON t.team_id = tlf.team_id
